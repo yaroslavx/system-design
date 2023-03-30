1017996a57e681d92facf0565f79b90b804fd0d8
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-03-30 16:55:58 +0300

    Implement some login logic

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -4,12 +4,12 @@ CREATE DATABASE auth;
 
 GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 
-USE auth
+USE auth;
 
 CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-    email VARCHAR(255) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
-)
+);
 
-INSERT INTO user (email, password) VALUES ("mail@mail.com", "12345");
+INSERT INTO user (email, password) VALUES ('mail@mail.com', '12345');
